b5b2cafd51503653f9ed20f1a987a96e334989d5
Ravi Shukla <rshuklabbb@gmail.com>
2022-03-01 18:24:41 +0530

    Added schemas for users, courses, likes, instructors and reviews

---
+++ server/src/models/review.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    review: {
+        type: String
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},{
+    versionKey: false,
+    timestamps: true
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
---
+++ server/src/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    cart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "course",
+        required: false
+    }],
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "course",
+        required: false
+    }],
+    purchased: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "course",
+        required: false
+    }],
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "review",
+        required: false
+    }]
+},{
+    versionKey: false,
+    timestamps: true
+});
+
+userSchema.pre('save', function(next) {
+    if ( !this.isModified('password') ) next();
+    this.password =  bcrypt.hashSync(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
