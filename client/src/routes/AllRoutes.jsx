05a777b838c7a495fc19e4e31b90e10bbfa124a9
Ravi Shukla <rshuklabbb@gmail.com>
2022-03-07 22:40:33 +0530

    feat: add routes for different pages

--- client/src/routes/AllRoutes.jsx
+++ client/src/routes/AllRoutes.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import {Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../../src/components/Home/Home';
+import HomePage from '../../src/pages/Home';
+import Search from '../../src/pages/Search';
+import Wishlist from '../../src/pages/Wishlist';
+import Cart from '../../src/pages/Cart';
+import Purchased from '../../src/pages/Purchased';
+import Course from '../../src/pages/Course';
+import Category from '../../src/pages/Category';
 import Auth from '../../src/components/Auth/Auth';
 
 import PostDetails from '../../src/components/PostDetails/PostDetails';
 import CreatorOrTag from '../../src/components/CreatorOrTag/CreatorOrTag';
 
 
-const AllRoutes = () =>{
-    const user = JSON.parse(localStorage.getItem('profile'));
+const AllRoutes = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
 
-    return(
-        <Switch>
-        <Route path="/" exact component={() => <Redirect to="/posts" />} />
-        <Route path="/posts" exact component={Home} />
-        <Route path="/posts/search" exact component={Home} />
-        <Route path="/posts/:id" exact component={PostDetails} />
-        <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
-        <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
-      </Switch>
-    )
+  return (
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/search" exact component={Search} />
+      <Route path="/course" exact component={Course} />
+      <Route path="/cart" exact component={Cart} />
+      <Route path="/wishlist" exact component={Wishlist} />
+      <Route path="/purchased" exact component={Purchased} />
+      <Route path="/category" exact component={Category} />
+      <Route path="/posts" exact component={Home} />
+      <Route path="/posts/search" exact component={Home} />
+      <Route path="/posts/:id" exact component={PostDetails} />
+      <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+      <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+    </Switch>
+  )
 }
 export default AllRoutes;
