1a3c94d9c34fa5c2937ab3c13fad8f25991b148b
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-03-10 22:45:01 +0530

    feat:Add Course Details Page, Add recommended post with course details page,refactor:Course Page

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+// app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 app.use('/cart', cartRouter);
